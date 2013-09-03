986ef4423d521f13a2c3f5a1563098a4d46c2e3a
Alex Birkett <alex@birkett.co.uk>
2013-09-03 22:59:06 +0200

    Created trackers api skeleton

--- routes/tracker.js
+++ routes/tracker.js
@@ -1,22 +1,42 @@
 var dbs  = require('../db')();
 
 module.exports = {
-    tracker: {
-        message: {
+    trackers: {
+        get: {
+            handler: function (req, res) {
+                res.send("return collection trackers associated with user");
+            }
+        },
+        put: {
+            handler: function (req, res) {
+                res.send("replace entire collection of trackers associated with user");
+            }
+        },
+        post:{
+            handler: function (req, res) {
+                res.send("create a new tracker and associated it with user");
+            }
+        },
+        delete: {
+            handler: function (req, res) {
+                res.send("delete entire collection associated with user");
+            }
+        },
+        ":id": {
             get: {
                 handler: function (req, res) {
-                    res.send("respond with a resource");
+                    res.send("retrieve a representation of " + req.params.id );
                 }
-            }
-        },
-        connected: {
-            get: function (req, res) {
-                res.send("respond with a resource");
-            }
-        },
-        disconnected: {
-            get: function (req, res) {
-                res.send("respond with a resource");
+            },
+            put: {
+                handler: function (req, res) {
+                    res.send("Replace " + req.params.id + ", or if it doesn't exist, create it.");
+                }
+            },
+            delete: {
+                handler: function (req, res) {
+                    res.send("Delete " + req.params.id);
+                }
             }
         }
     }
