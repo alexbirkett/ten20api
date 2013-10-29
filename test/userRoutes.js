08a802a29d805d99f0dfe61f0bf22210fd6f9c0d
Alex Birkett <alex@birkett.co.uk>
2013-10-29 08:45:14 +0100

    User route test case changes Use a clean db each run ('should' dumps entire object being tested to console, the response object is too big to be readable)

--- test/userRoutes.js
+++ test/userRoutes.js
@@ -1,11 +1,13 @@
 var should = require('should');
 var server = require('../server');
-var requestApi = require('request')
+var requestApi = require('request');
 var request = requestApi.defaults({followRedirect: false, jar: requestApi.jar()});
 
 var assert = require('assert');
 
 var port = 3001;
+
+var dbName = 'testUsers' + new Date().toUTCString().replace(/\s+/g,'');
 
 describe('user routes', function () {
     var url = 'http://localhost:' + port;
@@ -20,49 +22,56 @@ describe('user routes', function () {
         password: 'test2'
     };
 
-
     before(function (done) {
-        server.startServer(port, 'testdb', done);
+        server.startServer(port, dbName, done);
     });
 
     after(function (done) {
         server.close(done);
     });
 
-    it('should redirect to signin form before authenticated', function (done) {
+    it('should not be possible to access info endpoint', function (done) {
 
         request.get(url + '/user/info', function (error, response, body) {
             assert.ifError(error);
-            response.should.have.property('statusCode', 302);
-            response.headers.should.have.property('location', '/#signin');
+            assert.equal(403, response.statusCode);
             done();
         });
 
     });
 
-
-    it('should create an account success', function (done) {
+    it('should create an account', function (done) {
         request.post({url: url + '/signup', json: credential}, function (error, response, body) {
-            response.should.have.property('statusCode', 200);
-            response.body.should.have.property('message');
-            response.body.message.should.not.equal('server interal error!');
+            assert.ifError(error);
+            assert.equal(200, response.statusCode);
             done();
         });
     });
 
-    it('should still redirect to signin form before authenticated after account creation', function (done) {
+    it('should still not be possible to access info endpoint after create account', function (done) {
         request.get(url + '/user/info', function (error, response, body) {
             assert.ifError(error);
-            response.should.have.property('statusCode', 302);
-            response.headers.should.have.property('location', '/#signin');
+            console.log(response.body);
+            assert.equal(403, response.statusCode);
             done();
         });
 
     });
+
+   it('should fail to create account an second time', function (done) {
+        request.post({url: url + '/signup', json: credential}, function (error, response, body) {
+            assert.ifError(error);
+            assert.equal(403, response.statusCode);
+            done();
+        });
+    });
+
+
 
     it('should not signin with invalid credentials', function (done) {
         request.post({url: url + '/signin', json: invalidCredential}, function (error, response, body) {
-            response.should.have.property('statusCode', 200);
+            assert.ifError(error);
+            assert.equal(200, response.statusCode);
             response.body.should.have.property('message');
             response.body.message.should.equal('Invalid password');
             done();
@@ -70,12 +79,10 @@ describe('user routes', function () {
 
     });
 
-
-    it('should still redirect to signin form before authenticated after invalid login attempt', function (done) {
+    it('should still not be possible to access info endpoint after invalid login attempt', function (done) {
         request.get(url + '/user/info', function (error, response, body) {
             assert.ifError(error);
-            response.should.have.property('statusCode', 302);
-            response.headers.should.have.property('location', '/#signin');
+            assert.equal(403, response.statusCode);
             done();
         });
 
@@ -83,7 +90,8 @@ describe('user routes', function () {
 
     it('should signin with valid credentials', function (done) {
         request.post({url: url + '/signin', json: credential}, function (error, response, body) {
-            response.should.have.property('statusCode', 200);
+            assert.ifError(error);
+            assert.equal(200, response.statusCode);
             response.body.should.have.property('message');
             response.body.message.should.equal('');
             done();
@@ -91,9 +99,10 @@ describe('user routes', function () {
 
     });
 
-    it('should get user info success', function (done) {
+    it('should access info endpoint', function (done) {
         request.get(url + '/user/info', function (error, response, body) {
             assert.ifError(error);
+            assert.equal(200, response.statusCode);
             var body = JSON.parse(response.body);
             body.should.have.property('email');
             body.should.have.property('_id');
@@ -110,13 +119,13 @@ describe('user routes', function () {
         });
     });
 
-
-    it('should redirect to signin form after signout', function (done) {
+    it('should still not be possible to access info endpoint after sign out', function (done) {
         request.get(url + '/user/info', function (error, response, body) {
             assert.ifError(error);
-            response.should.have.property('statusCode', 302);
-            response.headers.should.have.property('location', '/#signin');
+            assert.equal(403, response.statusCode);
             done();
         });
+
     });
+
 });
