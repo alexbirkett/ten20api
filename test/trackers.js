5cc371917a48ca0e6d570b15d124b9325cdb94c2
Alex Birkett <alex@birkett.co.uk>
2013-10-31 22:43:32 +0100

    Trackers test cases Renamed test cases to fit with BDD style Added test cases for deleting trackers

--- test/trackers.js
+++ test/trackers.js
@@ -77,28 +77,28 @@ describe('trackers api', function () {
         server.close(done);
     });
 
-    it('GET should respond with 403 before login', function (done) {
+    it('should respond to GET with 403 before login', function (done) {
         request.get(url + '/trackers', function (error, response, body) {
             assert.equal(403, response.statusCode);
             done();
         });
     });
 
-    it('PUT should respond with 403 before login', function (done) {
+    it('should respond to PUT with 403 before login', function (done) {
         request.put(url + '/trackers', function (error, response, body) {
             assert.equal(403, response.statusCode);
             done();
         });
     });
 
-    it('POST should respond with 403 before login', function (done) {
+    it('should respond to POST with 403 before login', function (done) {
         request.post(url + '/trackers', function (error, response, body) {
             assert.equal(403, response.statusCode);
             done();
         });
     });
 
-    it('DELETE should respond with 403 before login', function (done) {
+    it('should respond to DELETE with 403 before login', function (done) {
         request.del(url + '/trackers', function (error, response, body) {
             assert.equal(403, response.statusCode);
             done();
@@ -113,32 +113,30 @@ describe('trackers api', function () {
         });
     });
 
-    it('should signin with valid credentials', function (done) {
+    it('setup: signin with credential1', function (done) {
         request.post({url: url + '/signin', json: credential1}, function (error, response, body) {
             assert.equal(200, response.statusCode);
             done();
         });
     });
 
-    it('GET should yield zero trackers', function (done) {
+    it('should respond to GET with zero trackers', function (done) {
         request.get(url + '/trackers', function (error, response, body) {
             assert.equal(200, response.statusCode);
-
             var jsonBody = JSON.parse(body);
-
-            jsonBody.should.have.lengthOf(0);
+            assert.equal(0, jsonBody.length);
             done();
         });
     });
 
-    it('POST should add tracker', function (done) {
+    it('should store tracker in response to POST', function (done) {
         request.post({url: url + '/trackers', json: trackers[0] }, function (error, response, body) {
             assert.equal(200, response.statusCode);
             done();
         });
     });
 
-    it('GET should yield posted tracker', function (done) {
+    it('should respond to GET with previously posted tracker', function (done) {
         request.get(url + '/trackers', function (error, response, body) {
             assert.equal(200, response.statusCode);
 
@@ -152,14 +150,14 @@ describe('trackers api', function () {
         });
     });
 
-    it('PUT /trackers should replace entire collection of trackers', function (done) {
+    it('should replace collection of trackers in response to PUT', function (done) {
         request.put({url: url + '/trackers', json: trackers }, function (error, response, body) {
             assert.equal(200, response.statusCode);
             done();
         });
     });
 
-    it('GET should yield posted trackers', function (done) {
+    it('should respond to GET with previously PUT collection of trackers', function (done) {
         request.get(url + '/trackers', function (error, response, body) {
             assert.equal(200, response.statusCode);
 
@@ -172,14 +170,14 @@ describe('trackers api', function () {
         });
     });
 
-    it('GET /trackers/ add document with id 526fb0b3970998723e000004 (does not exist)', function (done) {
+    it('should respond to GET for specific tracker id with 404', function (done) {
         request.get({url: url + '/trackers/526fb0b3970998723e000004' }, function (error, response, body) {
             assert.equal(404, response.statusCode);
             done();
         });
     });
 
-    it('PUT /trackers/526fb0b3970998723e000004 add document with id 526fb0b3970998723e000004', function (done) {
+    it('should store specific tracker ID in response to PUT', function (done) {
         request.put({url: url + '/trackers/526fb0b3970998723e000004', json: trackers[0] }, function (error, response, body) {
             assert.equal(200, response.statusCode);
             done();
@@ -187,7 +185,7 @@ describe('trackers api', function () {
     });
 
 
-    it('GET /trackers/ add document with id 526fb0b3970998723e000004', function (done) {
+    it('should respond to GET for specific tracker id with correct document', function (done) {
         request.get({url: url + '/trackers/526fb0b3970998723e000004' }, function (error, response, body) {
             assert.equal(200, response.statusCode);
             assert.equal(trackers[0].name, JSON.parse(body).name);
@@ -195,14 +193,14 @@ describe('trackers api', function () {
         });
     });
 
-    it('PUT /trackers/526fb0b3970998723e000004 replace document with id 526fb0b3970998723e000004', function (done) {
+    it('should replace specific document in response to PUT', function (done) {
         request.put({url: url + '/trackers/526fb0b3970998723e000004', json: trackers[1] }, function (error, response, body) {
             assert.equal(200, response.statusCode);
             done();
         });
     });
 
-    it('GET /trackers/ add document with id 526fb0b3970998723e000004', function (done) {
+    it('should respond to GET for specific tracker id with correct document with previously updated content', function (done) {
         request.get({url: url + '/trackers/526fb0b3970998723e000004' }, function (error, response, body) {
             assert.equal(200, response.statusCode);
             assert.equal(trackers[1].name, JSON.parse(body).name);
@@ -210,20 +208,54 @@ describe('trackers api', function () {
         });
     });
 
-    it('should logout', function (done) {
+    it('setup logout', function (done) {
         request.get(url + '/signout', function (error, response, body) {
             assert.ifError(error);
             done();
         });
     });
 
-    it('GET should respond with 403 after logout', function (done) {
+    it('should respond to GET with 403 after logout', function (done) {
         request.get(url + '/trackers', function (error, response, body) {
             assert.equal(403, response.statusCode);
             done();
         });
     });
 
+    it('should respond to PUT with 403 after logout', function (done) {
+        request.put(url + '/trackers', function (error, response, body) {
+            assert.equal(403, response.statusCode);
+            done();
+        });
+    });
+
+    it('should respond to POST with 403 after logout', function (done) {
+        request.post(url + '/trackers', function (error, response, body) {
+            assert.equal(403, response.statusCode);
+            done();
+        });
+    });
+
+    it('should respond to DELETE with 403 after logout', function (done) {
+        request.del(url + '/trackers', function (error, response, body) {
+            assert.equal(403, response.statusCode);
+            done();
+        });
+    });
+
+    it('should respond to GET requesting specific tracker with 403', function (done) {
+        request.get({url: url + '/trackers/526fb0b3970998723e000004' }, function (error, response, body) {
+            assert.equal(403, response.statusCode);
+            done();
+        });
+    });
+
+    it('should respond to DELETE specific tracker with 403', function (done) {
+        request.del({url: url + '/trackers/526fb0b3970998723e000004'}, function (error, response, body) {
+            assert.equal(403, response.statusCode);
+            done();
+        });
+    });
 
     it('setup: create second test account', function (done) {
         request.post({url: url + '/signup', json: credential2}, function (error, response, body) {
@@ -233,14 +265,14 @@ describe('trackers api', function () {
         });
     });
 
-    it('should signin with valid credentials', function (done) {
+    it('setup: signin with credential2', function (done) {
         request.post({url: url + '/signin', json: credential2}, function (error, response, body) {
             assert.equal(200, response.statusCode);
             done();
         });
     });
 
-    it('GET should yield zero trackers', function (done) {
+    it('should respond to GET with zero trackers (after switching accounts)', function (done) {
         request.get(url + '/trackers', function (error, response, body) {
             assert.equal(200, response.statusCode);
             var jsonBody = JSON.parse(body);
@@ -249,8 +281,22 @@ describe('trackers api', function () {
         });
     });
 
-    it('GET /trackers/ with id 526fb0b3970998723e000004 owned by other account should return 403', function (done) {
+    it('should respond to GET requesting tracker created by other account with 403', function (done) {
         request.get({url: url + '/trackers/526fb0b3970998723e000004' }, function (error, response, body) {
+            assert.equal(403, response.statusCode);
+            done();
+        });
+    });
+
+    it('should respond to PUT updating tracker created by other account with 403', function (done) {
+        request.put({url: url + '/trackers/526fb0b3970998723e000004', json: trackers[1] }, function (error, response, body) {
+            assert.equal(403, response.statusCode);
+            done();
+        });
+    });
+
+    it('should respond to DELETE tracker created by other account with 403', function (done) {
+        request.del({url: url + '/trackers/526fb0b3970998723e000004'}, function (error, response, body) {
             assert.equal(403, response.statusCode);
             done();
         });
@@ -263,4 +309,74 @@ describe('trackers api', function () {
         });
     });
 
+
+    it('setup logout', function (done) {
+        request.get(url + '/signout', function (error, response, body) {
+            assert.ifError(error);
+            done();
+        });
+    });
+
+    it('setup: signin with credential1', function (done) {
+        request.post({url: url + '/signin', json: credential1}, function (error, response, body) {
+            assert.equal(200, response.statusCode);
+            done();
+        });
+    });
+
+    it('should respond to GET for specific tracker id with correct document (survive delete attempts when logged out or by other account)', function (done) {
+        request.get({url: url + '/trackers/526fb0b3970998723e000004' }, function (error, response, body) {
+            assert.equal(200, response.statusCode);
+            assert.equal(trackers[1].name, JSON.parse(body).name);
+            done();
+        });
+    });
+
+    it('should respond to GET with 4 trackers', function (done) {
+        request.get(url + '/trackers', function (error, response, body) {
+            assert.equal(200, response.statusCode);
+            var jsonBody = JSON.parse(body);
+            assert.equal(4, jsonBody.length);
+            done();
+        });
+    });
+
+    it('should respond to DELETE tracker by removing tracker from database', function (done) {
+        request.del({url: url + '/trackers/526fb0b3970998723e000004'}, function (error, response, body) {
+            assert.equal(200, response.statusCode);
+            done();
+        });
+    });
+
+    it('should respond to GET for specific tracker id with 404', function (done) {
+        request.get({url: url + '/trackers/526fb0b3970998723e000004' }, function (error, response, body) {
+            assert.equal(404, response.statusCode);
+            done();
+        });
+    });
+
+    it('should respond to GET with 3 trackers', function (done) {
+        request.get(url + '/trackers', function (error, response, body) {
+            assert.equal(200, response.statusCode);
+            var jsonBody = JSON.parse(body);
+            assert.equal(3, jsonBody.length);
+            done();
+        });
+    });
+
+    it('should respond to DELETE tracker removing entire tracker collection', function (done) {
+        request.del({url: url + '/trackers'}, function (error, response, body) {
+            assert.equal(200, response.statusCode);
+            done();
+        });
+    });
+
+   it('should respond to GET with 0 trackers', function (done) {
+        request.get(url + '/trackers', function (error, response, body) {
+            assert.equal(200, response.statusCode);
+            var jsonBody = JSON.parse(body);
+            assert.equal(0, jsonBody.length);
+            done();
+        });
+    });
 });
