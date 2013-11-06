2d791cffaa2dc89a86641cf4a73b9f16442cd1b0
Alex Birkett <alex@birkett.co.uk>
2013-11-06 12:32:04 +0100

    Add trips collection

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var collectionApi = require('./lib/collection-api');
 
 
 var trackerRoute = collectionApi('tracker');
+var tripsRoute =  collectionApi('trip');
 
 var Ten20Api = function(app, db, io) {
    this.app = app;
@@ -30,11 +31,13 @@ Ten20Api.prototype.configureMiddleware = function() {
 
     this.io.sockets.on('connection', socket);
     configureDryRoutes(trackerRoute, this.app, '/trackers', ['use']);
+    configureDryRoutes(tripsRoute, this.app, '/trips', ['use']);
 };
 
 Ten20Api.prototype.configureRoutes = function () {
     this.app.post('/api/tracker/message', routes.index);
     configureDryRoutes(trackerRoute, this.app, '/trackers');
+    configureDryRoutes(tripsRoute, this.app, '/trips');
     configureDryRoutes(user, this.app);
 };
 
---
+++ test/trips.js
@@ -0,0 +1,1 @@
+require('./helper/authentication')('trips', 3005);
