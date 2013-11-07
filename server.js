5d533bc844b9e8b69e3e4a814e0024b5e11046b0
Alex Birkett <alex@birkett.co.uk>
2013-11-07 12:39:38 +0100

    Fix deprecation warnings https://github.com/senchalabs/connect/wiki/Connect-3.0

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ module.exports.startServer = function (port, dbUrl, configRoute, callback) {
             app.set('port', port);
             app.use(express.favicon());
             app.use(express.logger('dev'));
-            app.use(express.bodyParser());
+            app.use(express.urlencoded());
+            app.use(express.json());
             app.use(express.methodOverride());
 
             app.use(express.cookieParser('some secret'));
