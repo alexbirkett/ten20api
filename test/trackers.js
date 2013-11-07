4bdfa3dcc41ce833574c8592676880a85baaf409
Alex Birkett <alex@birkett.co.uk>
2013-11-07 22:10:35 +0100

    Added tests for indexes

--- test/trackers.js
+++ test/trackers.js
@@ -1,2 +1,31 @@
 
 require('./helper/authentication')('trackers', 3004);
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient
+
+var db;
+
+describe('test tracker indexes', function () {
+
+
+    before(function (done) {
+        MongoClient.connect('mongodb://localhost/testtrackers', function(err, adb) {
+            db = adb;
+            done(err);
+        });
+    });
+
+    after(function (done) {
+        if (db) {
+            db.close();
+        }
+        done();
+    });
+
+    it('should have user index on trackers collection', function (done) {
+        db.collection('trackers').indexes(function(err, indexes) {
+            assert.equal(indexes[1].key['user'], 1);
+            done();
+        });
+    });
+});
--- test/trips.js
+++ test/trips.js
@@ -1,1 +1,38 @@
 require('./helper/authentication')('trips', 3005);
+
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient
+
+var db;
+
+describe('test trip indexes', function () {
+
+
+    before(function (done) {
+        MongoClient.connect('mongodb://localhost/testtrips', function(err, adb) {
+            db = adb;
+            done(err);
+        });
+    });
+
+    after(function (done) {
+        if (db) {
+            db.close();
+        }
+        done();
+    });
+
+    it('should have user index on trips collection', function (done) {
+        db.collection('trips').indexes(function(err, indexes) {
+            assert.equal(indexes[1].key['user'], 1);
+            done();
+        });
+    });
+
+    it('should have tracker index on trips collection', function (done) {
+        db.collection('trips').indexes(function(err, indexes) {
+            assert.equal(indexes[2].key['tracker'], 1);
+            done();
+        });
+    });
+});
