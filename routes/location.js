fe32668dfbd729285348f07be39c0545901b17a8
Alex Birkett <alex@birkett.co.uk>
2013-11-25 21:20:25 +0100

    renamed location_update route to location/update_by_serial

---
+++ routes/location.js
@@ -0,0 +1,33 @@
+var db = require('../lib/db');
+
+var getObjectCollection = function () {
+    return db.getDb().collection('trackers');
+};
+
+var updateTracker = function (serial, location, callback) {
+    var query = { serial: serial};
+    var data = { $set: location };
+    getObjectCollection().update(query, data, callback);
+};
+
+module.exports =
+{
+    location: {
+        update_by_serial: {
+            ":id": {
+                post: function (req, res) {
+                    console.log(req.params.id);
+
+                    updateTracker(req.params.id, req.body, function (err) {
+                        if (err) {
+                            res.json(500, {});
+                        } else {
+                            res.json({});
+                        }
+
+                    });
+                }
+            }
+        }
+    }
+};
