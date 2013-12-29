919652ecee0235843e39f3827ba3fb9c209cf9ad
Alex Birkett <alex@birkett.no>
2013-12-30 00:23:47 +0100

    log error message when server started

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var server = require('./server')
 var configRoute = require('./lib/route-config');
 
-server.startServer(3001, 'mongodb://localhost/ten20api', configRoute, function() {
-
+server.startServer(3001, 'mongodb://localhost/ten20api', configRoute, function(err) {
+    console.log('servar started ' + err);
 });
