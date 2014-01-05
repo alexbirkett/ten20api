96ce11187b40bdf85674ee9ea75d2a76cd46707b
Alex Birkett <alex@birkett.no>
2014-01-05 08:14:26 +0100

    Configure port and dbname using optimist

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var server = require('./server')
 var configRoute = require('./lib/route-config');
 
-server.startServer(3001, 'mongodb://localhost/ten20api', configRoute, function(err) {
-    console.log('servar started ' + err);
+var optimist = require('optimist');
+var argv = optimist.usage('Usage: $0  --dbname [string] --port [num]').
+    options('d', {
+        alias: 'dbname',
+        describe: 'database name'
+    }).
+    options('p', {
+        alias: 'port',
+        describe: 'port'
+    }).
+    default('p', 3001).
+    default('d', 'ten20api')
+    .argv;
+
+server.startServer(argv.port, 'mongodb://localhost/' + argv.dbname, configRoute, function(err) {
+    console.log('servar started on port ' + argv.port + ' dbname ' + argv.dbname);
 });
