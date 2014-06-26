bab257a48abfc88cb302f8690c67fcbe256a240e
Alex Birkett <alex@birkett.co.uk>
2014-06-26 20:11:23 +0200

    Log error codes Report error in callback

--- test/helper/auth.js
+++ test/helper/auth.js
@@ -7,7 +7,7 @@ module.exports = function (url, request) {
         signUp: function (credential, callback) {
             request.post({url: url + '/signup', json: credential}, function (error, response, body) {
                 if (response.statusCode !== 200) {
-                    error = 'non 200 status code';
+                    error = 'non 200 status code: ' + response.statusCode;
                 }
                 callback(error, response, body);
             });
@@ -15,7 +15,7 @@ module.exports = function (url, request) {
         signIn: function (credential, callback) {
             request.post({url: url + '/signin', json: credential}, function (error, response, body) {
                 if (response.statusCode !== 200) {
-                    error = 'non 200 status code';
+                    error = 'non 200 status code: ' + response.statusCode;
                 }
                 callback(error, response, body);
             });
@@ -27,14 +27,19 @@ module.exports = function (url, request) {
         },
         authenticate: function(credential, callback) {
             request.post({url: url + '/authenticate', json: credential}, function (error, response, body) {
-                if (!error && response.statusCode == 200 && body.token) {
-                    var request = requestApi.defaults({followRedirect: false, headers: {
-                        'Authorization': 'Bearer ' + body.token
-                    }});
-                    callback(undefined, request);
+                if (response.statusCode == 200) {
+                    if (!error && body.token) {
+                        var request = requestApi.defaults({followRedirect: false, headers: {
+                            'Authorization': 'Bearer ' + body.token
+                        }});
+                        callback(undefined, request);
+                    } else {
+                        callback(error);
+                    }
                 } else {
-                    callback(error);
+                    callback('non 200 status code - ' + response.statusCode);
                 }
+
             });
         },
         getUserInfo: function(callback) {
